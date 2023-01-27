392e578bebd0e6774f18586d3d2b2252000fd630
Mohit Kadwe <118180431+mohitkadwe@users.noreply.github.com>
2023-01-27 23:16:46 +0530

    📦 NEW: concatenated-words

---
+++ concatenated-words.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  map<string, int> m;
+  bool cal(string &str, int idx, int cnt)
+  {
+    if (idx == str.size())
+      return cnt > 1; // check atleat two string are concate
+    string tmp = "";
+    for (int i = idx; i < str.size(); i++)
+    {
+      tmp += str[i];
+      if (m.find(tmp) != m.end())
+      {
+        // if string found then check with next index
+        bool flag = cal(str, i + 1, cnt + 1);
+        if (flag)
+          return true;
+      }
+    }
+    return false;
+  }
+
+  vector<string> findAllConcatenatedWordsInADict(vector<string> &words)
+  {
+    for (auto x : words)
+    {
+      m[x]++;
+    }
+    vector<string> ans;
+    for (auto x : words)
+    {
+      bool flag = cal(x, 0, 0);
+      // if given string satisfy the condition then add it to our answer
+      if (flag)
+      {
+        ans.push_back(x);
+      }
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<string> words = {"cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat"};
+  vector<string> ans = s.findAllConcatenatedWordsInADict(words);
+  for (auto &s : ans)
+    cout << s << " ";
+  cout << endl;
+  return 0;
+}
