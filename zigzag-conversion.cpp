49d94829ae783662174c52d2c9fad12bd99a6688
Mohit Kadwe <118180431+mohitkadwe@users.noreply.github.com>
2023-02-03 23:46:56 +0530

    📦 NEW: zigzag-conversion

---
+++ zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+  string convert(string s, int numRows)
+  {
+    if (numRows == 1)
+    {
+      return s;
+    }
+    string res;
+    int n = s.size();
+    int cycleLen = 2 * numRows - 2;
+    for (int i = 0; i < numRows; i++)
+    {
+      for (int j = 0; j + i < n; j += cycleLen)
+      {
+        res += s[j + i];
+        if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+          res += s[j + cycleLen - i];
+      }
+    }
+    return res;
+  }
+};
+
+int main()
+{
+  Solution s;
+  cout << s.convert("PAYPALISHIRING", 3) << endl;
+  return 0;
+}
