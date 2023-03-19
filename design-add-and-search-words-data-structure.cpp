d06cbf37e59a2bcbd430e39b426850e494906d3e
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-03-19 23:45:37 +0530

    📦 NEW: design-add-and-search-words-data-structure

---
+++ design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class WordDictionary
+{
+public:
+  vector<string> v;
+  WordDictionary()
+  {
+  }
+
+  void addWord(string word)
+  {
+    v.push_back(word);
+  }
+
+  bool search(string word)
+  {
+    int count = 0;
+    int n = word.size();
+    for (int i = 0; i < v.size(); i++)
+    {
+      if (n == v[i].size())
+      {
+        for (int j = 0; j < n; j++)
+        {
+          if (v[i][j] == word[j] || word[j] == '.')
+          {
+            count++;
+          }
+          else
+          {
+            count = 0;
+            break;
+          }
+        }
+        if (count == n)
+          return true;
+      }
+    }
+    return false;
+  }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
+
+int main()
+{
+  WordDictionary *obj = new WordDictionary();
+  obj->addWord("bad");
+  obj->addWord("dad");
+  obj->addWord("mad");
+  cout << obj->search("pad") << endl;
+  cout << obj->search("bad") << endl;
+  cout << obj->search(".ad") << endl;
+  cout << obj->search("b..") << endl;
+  return 0;
+}
