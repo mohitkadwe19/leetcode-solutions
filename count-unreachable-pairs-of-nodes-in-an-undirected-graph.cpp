ec9295303bc4aa0338d593ccce9e5708a3b38e06
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-03-25 22:25:49 +0530

    📦 NEW: count-unreachable-pairs-of-nodes-in-an-undirected-graph

---
+++ count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+class Solution
+{
+public:
+  void dfs(vector<int> adj[], int node, vector<int> &vis, int &c)
+  {
+    c++;
+    vis[node] = 1;
+    for (auto a : adj[node])
+    {
+      if (!vis[a])
+      {
+        dfs(adj, a, vis, c);
+      }
+    }
+  }
+  long long countPairs(int n, vector<vector<int>> &ed)
+  {
+    vector<int> adj[n];
+    for (auto a : ed)
+    {
+      adj[a[0]].push_back(a[1]);
+      adj[a[1]].push_back(a[0]);
+    }
+    vector<int> vis(n, 0);
+    long long ans = 0;
+    ll su = 0;
+    int c = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (!vis[i])
+      {
+        dfs(adj, i, vis, c);
+        ans += su * c;
+        su += c;
+        c = 0;
+      }
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+  Solution s;
+  vector<vector<int>> edges = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {1, 5}};
+  cout << s.countPairs(5, edges) << endl;
+  return 0;
+}
