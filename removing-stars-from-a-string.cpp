97e8978cb7aeeba1d7e6bbee5c10de9f14127637
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-04-11 23:39:52 +0530

    📦 NEW: removing-stars-from-a-string

---
+++ removing-stars-from-a-string.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  string removeStars(string s)
+  {
+    stack<char> stk;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+      char cur = s[i];
+      if (cur == '*')
+      {
+        stk.pop();
+      }
+      else
+      {
+        stk.push(cur);
+      }
+    }
+    string ans = "";
+    while (!stk.empty())
+    {
+      ans += stk.top();
+      stk.pop();
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};
+
+int main()
+{
+  string s = "leet**cod*e";
+  Solution obj;
+  cout << obj.removeStars(s) << endl;
+  return 0;
+}
