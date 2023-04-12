10530dffc910d605b6ead049ec22e15cbf95e881
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-04-12 23:26:09 +0530

    📦 NEW: simplify-path

---
+++ simplify-path.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  string simplifyPath(string path)
+  {
+    int n = path.size();
+    for (int i = 0; i < n; ++i)
+      if (path[i] == '/')
+      {
+        path[i] = ' ';
+      }
+    vector<string> v;
+    stringstream s(path);
+    string word;
+    while (s >> word)
+    {
+      if (word == "..")
+      {
+        if (v.empty())
+        {
+          continue;
+        }
+        else
+        {
+          v.pop_back();
+        }
+      }
+      else if (word == ".")
+      {
+        continue;
+      }
+      else
+      {
+        v.push_back(word);
+      }
+    }
+    string res = "/";
+    for (auto it : v)
+    {
+      res += it + "/";
+    }
+
+    if (res.back() == '/' && res.size() > 1)
+    {
+      res.pop_back();
+    }
+    return res;
+  }
+};
+
+int main()
+{
+  string path = "/home/";
+  Solution s;
+  cout << s.simplifyPath(path) << endl;
+  return 0;
+}
