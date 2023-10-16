afdcd47ad299ccb5f60709e331bcfa696e38b8af
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-10-16 19:17:44 +0530

    📦 NEW: pascals-triangle-ii

---
+++ pascals-triangle-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<>();
+        res.add(1);
+        long prev = 1;
+        for (int k = 1; k <= rowIndex; k++) {
+            long next_val = prev * (rowIndex - k + 1) / k;
+            res.add((int) next_val);
+            prev = next_val;
+        }
+        return res;   
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        System.out.println(sol.getRow(3));
+    }
+}
