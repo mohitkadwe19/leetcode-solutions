5441543e274b74a6980ec63dc8c99c7d1dd4fb70
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2023-10-21 19:19:50 +0530

    📦 NEW: constrained-subsequence-sum

---
+++ constrained-subsequence-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int constrainedSubsetSum(int[] nums, int k) {
+       PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            return b[0] - a[0];
+        });
+        
+        heap.add(new int[] {nums[0], 0});
+        int ans = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            while (i - heap.peek()[1] > k) {
+                heap.remove();
+            }
+            
+            int curr = Math.max(0, heap.peek()[0]) + nums[i];
+            ans = Math.max(ans, curr);
+            heap.add(new int[] {curr, i});
+        }
+        
+        return ans;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {10,2,-10,5,20};
+        int k = 2;
+        System.out.println("constrainedSubsetSum: " + sol.constrainedSubsetSum(nums, k));
+    }
+}
